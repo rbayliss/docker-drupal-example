f3362df29ca1b1cd34df4359c578258af3f62ea7
Rob Bayliss <rob@lastcallmedia.com>
2015-04-16 16:52:45 -0400

    Rename solr container

--- install.sh
+++ install.sh
@@ -2,8 +2,9 @@
 
 drush si -y
 
-drush vset site_name 'Docker Demo Site'
+drush vset site_name 'Docker Demo Site' -y
 
 # Enable page caching so we can demonstrate Varnish
-drush vset cache 1
+drush vset cache 1 -y
 
+drush en search_api_solr -y
